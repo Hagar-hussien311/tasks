fa20d4f86c9a2bb59853e8b194ce114d8f52c0ce
Hagar-hussien311 <hagarhussien210@gmail.com>
2022-02-09 23:50:22 +0200

    second task

--- task1/root.php
+++ task1/root.php
@@ -9,7 +9,7 @@ if ($_POST) {
 <html lang="en">
 
 <head>
-    <title>calculator</title>
+    <title>Root</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
